9432daf92f316264752a0a43e8453aa267ce5eae
Colin Gillespie <csgillespie@gmail.com>
2016-03-26 20:45:51 +0000

    A few tweaks to the help page.

--- R/get_all_typeforms.R
+++ R/get_all_typeforms.R
@@ -1,11 +1,17 @@
 #' Fetch all available typeforms
 #'
-#' Returns a data frame containing your typeforms and their UIDs.
+#' This functions returns a data frame containing your typeforms and their
+#' associated UIDs.
 #' @importFrom jsonlite fromJSON
 #' @param api Your private api key.
+#' @return A two column data frame
 #' @export
+#' @examples
+#' \dontrun{
+#' api = "XXXXX"
+#' get_all_typeforms(api)
+#' }
 get_all_typeforms = function(api) {
-
   url = paste0("https://api.typeform.com/v1/forms?key=", api)
   jsonlite::fromJSON(url)
 }
